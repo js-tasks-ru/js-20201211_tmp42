494b815c06ccd2ab244522d39f4ef93a8c3ea001
andrei.chebasov <andrei.chebasov@travelline.ru>
2020-12-15 22:26:19 +0300

    выполнил 2,3,4 задание

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,12 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  let newArr = arr.slice();
+  let sort;
+  if (param == 'asc') {
+    sort = newArr.sort((a, b) => a.localeCompare(b, undefined, {caseFirst: 'upper'}));
+  } else if (param == 'desc') {
+    sort = newArr.sort((a, b) => b.localeCompare(a, undefined, {caseFirst: 'upper'}));
+  }
+  return sort;
 }
